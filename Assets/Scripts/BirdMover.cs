25db37719850d7cef3a8be04a3c9627ac2aee9d6
theSkyKV <theSkyKV@gmail.com>
2021-03-01 16:42:50 +0300

    Create

---
+++ Assets/Scripts/BirdMover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BirdMover : MonoBehaviour
+{
+    [SerializeField] private Transform _startPosition;
+    [SerializeField] private float _movementSpeed = 1;
+    [SerializeField] private float _jumpForce = 250;
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _minRotationZ;
+    [SerializeField] private float _maxRotationZ;
+
+    private Rigidbody2D _rigidbody;
+    private Quaternion _minRotation;
+    private Quaternion _maxRotation;
+
+    private void Start()
+    {
+        transform.position = _startPosition.position;
+
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.velocity = Vector2.zero;
+
+        _minRotation = Quaternion.Euler(0, 0, _minRotationZ);
+        _maxRotation = Quaternion.Euler(0, 0, _maxRotationZ);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody.velocity = new Vector2(_movementSpeed, 0);
+            transform.rotation = _maxRotation;
+            _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Force);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, _minRotation, _rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/BirdTracker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdTracker : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _xOffset;
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(_target.position.x + _xOffset, transform.position.y, transform.position.z);
+    }
+}
